558c4673f1efd849817533f87b4106093da5e6f3
Morten Olsen <fbtijfdq@void.black>
2023-11-10 20:16:42 +0100

    update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,16 @@
 ENV=${ENV:-dev}
-CERT=${CERT:-stage}
-DOMAIN=${DOMAIN:-loopback.services}
+DOMAIN=${DOMAIN:-olsen.cloud}
+CERT=${CERT:-prod}
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm dependency build "$SCRIPT_DIR/../charts/core/argo-cd"
 
-kind delete cluster
-kind create cluster --config "$SCRIPT_DIR/../kind-config.yaml"
 "$SCRIPT_DIR/update-secrets.sh"
 helm install \
   --set env=$ENV \
-  --set domain=$DOMAIN \
   --set issuer=letsencrypt-$CERT \
+  --set domain=$DOMAIN \
   --wait argo-cd "$SCRIPT_DIR/../charts/core/argo-cd"
 helm template "$SCRIPT_DIR/../apps" \
   --set env=$ENV \
