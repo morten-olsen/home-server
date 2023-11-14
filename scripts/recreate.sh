618660539f8fe82a70c1945f50bf0b178056f45a
Morten Olsen <fbtijfdq@void.black>
2023-11-14 22:38:00 +0100

    update

--- scripts/recreate.sh
+++ scripts/recreate.sh
@@ -1,21 +1,4 @@
-ENV=${ENV:-dev}
-CERT=${CERT:-stage}
-DOMAIN=${DOMAIN:-loopback.services}
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-helm repo add argo-cd https://argoproj.github.io/argo-helm
-helm dependency build "$SCRIPT_DIR/../charts/core/argo-cd"
 
 kind delete cluster
 kind create cluster --config "$SCRIPT_DIR/../kind-config.yaml"
-"$SCRIPT_DIR/update-secrets.sh"
-helm install \
-  --set env=$ENV \
-  --set domain=$DOMAIN \
-  --set issuer=letsencrypt-$CERT \
-  --wait argo-cd "$SCRIPT_DIR/../charts/core/argo-cd"
-helm template "$SCRIPT_DIR/../apps" \
-  --set env=$ENV \
-  --set issuer=letsencrypt-$CERT \
-  --set domain=$DOMAIN \
-    | kubectl apply -f -
